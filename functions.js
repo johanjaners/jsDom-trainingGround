5484359615f0cd4e772fa147faa857076251246e
Johan Janérs <johanjaners@gmail.com>
2025-10-02 17:09:10 +0200

    functtions.js :addListInMain added , inde last <script>: addListInMain(10)

--- functions.js
+++ functions.js
@@ -2,5 +2,20 @@ myFunctions = {
     getCompanyName() {
         return "SALT";
     },
-},
+};
+
+function addListInMain(numberOfListItems) {
+    const ulElement = document.createElement("ul");
+
+    for(let i = 0; i < numberOfListItems; i++) {
+        const liItem = document.createElement("li");
+        liItem.textContent = `Item #${i + 1}`;
+
+        ulElement.appendChild(liItem);
+    }
+
+    const main = document.querySelector("main");
+    main.appendChild(ulElement);
+}
+
 console.log("salt> Functions loaded");
