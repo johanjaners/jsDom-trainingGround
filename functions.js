ee72602410f0eb1ca5cec43ed9dd7f4a5de085d3
Johan Janérs <johanjaners@gmail.com>
2025-10-02 19:31:18 +0200

    changingCSSwithJS: <script> added function addResultStyling

--- functions.js
+++ functions.js
@@ -26,5 +26,27 @@ function removeDivById(id) {
     }
 }
 
+function addStyleBasedOfContent(element, content) {
+    const convertedNumber = Number(content);
+
+    if (Number.isNaN(convertedNumber)) {
+        return;
+    }
+    if (convertedNumber < 0) {
+        element.classList.add("negativeResult");
+        return;
+    }
+    element.classList.add("positiveResult");
+}
+
+function addResultStyling() {
+    const mainResultElements = document.querySelectorAll("mainResult");
+
+    for(let i = 0; i < mainResultElements.length; i++) {
+        const content = mainResultElements[i].textContent;
+
+        addStyleBasedOfContent(mainResultElements[i], content);
+    }
+}
 
 console.log("salt> Functions loaded");
